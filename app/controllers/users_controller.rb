037005673f83bf5a0520f47bbf39a69c46186c9a
CCHINCHEE <CHONGCC90@GMAIL.COM>
2018-02-28 18:54:40 +0800

    added sign up and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,59 @@
 class UsersController < ApplicationController
+	include ApplicationHelper
+	
+	before_action :find_user, only: [:show, :edit, :update] 
+	before_action :require_login, only: [:show, :edit]
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to "/users/#{@user.id}"
+		else
+			redirect_to root_path
+		end	
+	end	
+
+	def login
+		@user = User.find_by(email: params[:session][:email])
+		if @user != nil && @user.authenticate(params[:session][:password])
+			session[:user_id] = @user.id
+
+			redirect_to "/users/#{@user.id}"
+
+		else
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:notice] = "Signed out sucessfully."
+     	redirect_to '/'
+	end
+
+
+
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :password, :email)
+	end
+
+	def find_user
+		if @user = User.find_by(id: params[:id])
+			return @user
+		else
+			redirect_to "/", :flash => { :errors => "Error!!!!"}	
+		end
+	end
+
+	def require_login
+		if !logged_in?
+			redirect_to root_path
+		end	
+	end
+
+	
+
 end
